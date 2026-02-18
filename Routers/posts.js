a881d17f1c89170d27b507ee19cb9c41b150c993
francesco.miliotti <francesco.miliotti@email.com>
2026-02-18 15:55:45 +0100

    creo cartella router + rotte

--- Routers/posts.js
+++ Routers/posts.js
@@ -1,15 +1,11 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const router = express.Router();
 
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-res.send('Benvenuto nel Server del mio blog')
-})
 
-app.get('/Bacheca/', (req, res) => {
-	const bacheca = [
+
+router.get('/', (req, res) => {
+	const posts = [
 		{
 			title: "Orto",
 			contents: "Scheda coltivazione degli ortaggi",
@@ -43,12 +39,33 @@ app.get('/Bacheca/', (req, res) => {
         
 	];
 
-	res.json(bacheca);
+	res.json(posts);
 })
 
 
-
-
-app.listen(port, () => {
-console.log(`Example app listening on port ${port}`)
+//Show (cRud)
+router.get('/:id', (req, res) => {
+	res.send(`You requested to SHOW the post with id: ${req.params.id}`);
 })
+
+//Store (Crud)
+router.post('/', (req, res) => {
+	res.send(`You request to CREATE a new post`);
+})
+
+//Update (crUd)
+router.put('/:id', (req, res) => {
+	res.send(`You requested to UPDATE (complete) the post with id: ${req.params.id}`);
+})
+
+//Modify (crUd)
+router.patch('/:id', (req, res) => {
+	res.send(`You requested to MODIFY (partial) the post with id: ${req.params.id}`);
+})
+
+//Destroy (cruD)
+router.delete('/:id', (req, res) => {
+	res.send(`You requested to DELETE the post with id: ${req.params.id}`);
+})
+
+module.exports = router;
