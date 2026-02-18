b48d87bb38c933eb7c23173a86bb681535bc9a74
francesco.miliotti <francesco.miliotti@email.com>
2026-02-18 15:58:19 +0100

    imposto id sul file dei post

--- Routers/posts.js
+++ Routers/posts.js
@@ -7,30 +7,35 @@ const router = express.Router();
 router.get('/', (req, res) => {
 	const posts = [
 		{
+            id: 1,
 			title: "Orto",
 			contents: "Scheda coltivazione degli ortaggi",
             image: "/bacheca/Orto.jpg",
 			tags: ["pomodoro", "zucchina", "melanzana"],
 		}, 
         {
+            id: 2,
 			title: "Frutti",
 			contents: "Guida coltivazione alberi da Frutto",
             image: "/bacheca/Frutti.jpg",
 			tags: ["albicocco", "pesco", "melo"],
 		},
         {
+            id: 3,
 			title: "Erbe",
 			contents: "Guida alla scoperta delle erbe aromatiche",
             image: "/bacheca/aromatiche.jpg",
 			tags: ["basilico", "menta", "rosmarino"],
 		},
         {
+            id: 4,
 			title: "Difesa",
 			contents: "Guida alla difesa dalle avversità",
             image: "/bacheca/difesa.jpg",
 			tags: ["malattie", "insetti", "animali"],
 		},
         {
+            id: 5,
 			title: "Guida",
 			contents: "Consigli utili alla coltivazione",
             image: "/bacheca/guida.jpg",
