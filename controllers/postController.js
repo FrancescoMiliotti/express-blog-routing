59df2a08d20ea45abcfe0ed74849c074191d6132
francesco.miliotti <francesco.miliotti@email.com>
2026-02-24 15:58:16 +0100

    creo controller dei post

---
+++ controllers/postController.js
@@ -0,0 +1,53 @@
+function index (req, res) {
+
+	const posts = [
+		{
+            id: 1,
+			title: "Orto",
+			contents: "Scheda coltivazione degli ortaggi",
+            image: "/bacheca/Orto.jpg",
+			tags: ["pomodoro", "zucchina", "melanzana"],
+		}, 
+        {
+            id: 2,
+			title: "Frutti",
+			contents: "Guida coltivazione alberi da Frutto",
+            image: "/bacheca/Frutti.jpg",
+			tags: ["albicocco", "pesco", "melo"],
+		},
+        {
+            id: 3,
+			title: "Erbe",
+			contents: "Guida alla scoperta delle erbe aromatiche",
+            image: "/bacheca/aromatiche.jpg",
+			tags: ["basilico", "menta", "rosmarino"],
+		},
+        {
+            id: 4,
+			title: "Difesa",
+			contents: "Guida alla difesa dalle avversità",
+            image: "/bacheca/difesa.jpg",
+			tags: ["malattie", "insetti", "animali"],
+		},
+        {
+            id: 5,
+			title: "Guida",
+			contents: "Consigli utili alla coltivazione",
+            image: "/bacheca/guida.jpg",
+			tags: ["suolo", "raccolta", "potatura"]
+		}
+        
+	];
+
+	res.json(posts);
+    console.log("Chiamata GET file JSON ricevuta");
+    
+    
+}
+
+const oggettone = {
+
+    index
+}
+
+module.exports = oggettone
